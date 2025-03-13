1ee4df7a885a695bc880d2e4242d1f98b2a82fdd
Bhanuprabhas1234 <bhanuprabhastamminedi@gmail.com>
2025-03-13 11:09:54 +0000

    updated hello.cpp with changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "Changes have been made \n" ;
+  cout << "These changes are done as part of the task-4 \n" ;
   return 0;
 }
